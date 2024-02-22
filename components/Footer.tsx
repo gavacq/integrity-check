8d0c70bd1b335e387e8e6aaad87ed78f0a3db342
gavacq <8844891+gavacq@users.noreply.github.com>
2024-02-21 22:49:58 -0800

    add icon tray

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,27 @@
+'use client'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartSimple, faHouse, faLayerGroup } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import { useState } from 'react';
+import { usePathname } from 'next/navigation'
+
 
 const Footer = () => {
+  const pathname = usePathname();
+  const [activeTab, setActiveTab] = useState(pathname.replace('/', ''));
+  console.log(pathname)
+
   return (
 <div className="flex justify-around items-center text-shuttle-gray-200 py-3 sticky bottom-0 w-full bg-ebony-950 h-24">
-  <Link href="/analytics">
-    <FontAwesomeIcon icon={faChartSimple} size="lg" />
+  <Link href="/analytics" onClick={() => setActiveTab('analytics')}>
+    <FontAwesomeIcon icon={faChartSimple} size="2xl" className={`${activeTab === 'analytics' ? 'text-blue-charcoal-500': 'text-shuttle-gray-200'}`}/>
   </Link>
-  <Link href="/">
-      <FontAwesomeIcon icon={faHouse} size="lg" />
+  <Link href="/" onClick={() => setActiveTab('home')}>
+    <FontAwesomeIcon icon={faHouse} size="2xl" className={`${activeTab === 'home' ? 'text-blue-charcoal-500': 'text-shuttle-gray-200'}`}/>
   </Link>
-  <Link href="/categories">
-      <FontAwesomeIcon icon={faLayerGroup} size="lg" />
+  <Link href="/categories" onClick={() => setActiveTab('categories')}>
+    <FontAwesomeIcon icon={faLayerGroup} size="2xl" className={`${activeTab === 'categories' ? 'text-blue-charcoal-500': 'text-shuttle-gray-200'}`}/>
   </Link>
 </div>
   );
