426876ae133c700aed306554ed8d2c47017e9b5f
gavacq <8844891+gavacq@users.noreply.github.com>
2024-02-26 14:15:26 -0800

    add logging

--- app/login/LoginButton.tsx
+++ app/login/LoginButton.tsx
@@ -1,18 +1,53 @@
 'use client'
-import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithRedirect, GoogleAuthProvider, getRedirectResult } from "firebase/auth";
 import { firebaseApp } from "utils/firebase";
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect } from "react";
 
 const LoginButton = () => {
+  const auth = getAuth(firebaseApp);
 
   const handleLogin = () => {
-    const auth = getAuth(firebaseApp);
     const provider = new GoogleAuthProvider();
     signInWithRedirect(auth, provider);
   }
+
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then((result) => {
+        if (!result) {
+          console.warn('No redirect result');
+          return
+        }
+
+        // This gives you a Google Access Token. You can use it to access Google APIs.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+        console.log('User:', user);
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.error('getRedirectResult error:', errorCode, errorMessage, email, credential);
+        // ...
+      });
+    }, [auth]);
+
   
   return (
-    <button onClick={() => handleLogin()} className="bg-gradient-to-r from-revolver-800 to-ebony-800 text-white font-bold py-2 px-4 rounded h-10 w-40 flex items-center justify-center">
-      Login
+    <button onClick={() => handleLogin()} className="bg-gradient-to-r from-revolver-800 to-ebony-800 text-white font-bold py-2 px-4 rounded h-10 w-75 flex items-center justify-center">
+      <FontAwesomeIcon icon={faGoogle} className="mr-2" />
+      Sign-in with Google
     </button>
   )
 }
