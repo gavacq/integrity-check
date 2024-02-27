67e9493b6ec97808de28d12dd3bbafa264a1556f
gavacq <8844891+gavacq@users.noreply.github.com>
2024-02-26 16:40:13 -0800

    add name to menu

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -38,12 +38,15 @@ const ProfileMenu = () => {
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50" onClick={() => setShowMenu(false)}>
           <div className="bg-ebony-950 w-fit h-48 rounded-lg absolute top-4 right-4 flex flex-col justify-center items-center px-6 space-y-2" onClick={e => e.stopPropagation()}>
 
+            {currentUser && (
+              <h3 className="text-lunar-green-400">Hello, {currentUser.displayName}</h3>
+            )}
             <Link href="/profile" className="text-white w-full text-center my-2 py-1 px-2
             bg-revolver-900 rounded-md" onClick={() => setShowMenu(false)}>
               Profile
             </Link>
             {currentUser ? (
-              <button className="text-lunar-green-300 my-2 py-1 px-2 bg-revolver-900 rounded-md" onClick={() => { handleSignOut()}}>Sign Out</button>) : (
+              <button className="text-white w-full my-2 py-1 px-2 bg-revolver-900 rounded-md" onClick={() => { handleSignOut()}}>Sign Out</button>) : (
               <button onClick={() => handleLogin()} className="bg-gradient-to-r from-revolver-800 to-ebony-800 text-white font-bold py-2 px-4 rounded h-10 w-75 flex items-center justify-center">
                 <FontAwesomeIcon icon={faGoogle} className="mr-2" />
                 Sign-in with Google
