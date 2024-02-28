f0bd99be4c8fe01c1482968890dbe25fcbf8a31e
gavacq <8844891+gavacq@users.noreply.github.com>
2024-02-27 20:58:03 -0800

    wip new category inputs inline with table

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -77,69 +77,74 @@ const Categories = () => {
     <div className='grid grid-cols-3 items-center w-2/3'>
       <h1 className="col-start-2 text-lunar-green-300 text-center">Categories</h1>
       <div className='text-right'>
-        <FontAwesomeIcon icon={faPlus} size="lg" className='text-lunar-green-300 cursor-pointer' onClick={() => HandleClickAddCategory()} />
+        <FontAwesomeIcon icon={faPlus} size="lg" className='text-lunar-green-200 cursor-pointer' onClick={() => HandleClickAddCategory()} />
       </div>
     </div>
     <div className='flex flex-col mt-4 w-2/3'>
-      {!showInputs && (
         <div className='grow flex flex-col items-center'>
             <table className='text-lunar-green-300 border-collapse w-full'>
               <thead>
                 <tr className='border-b border-lunar-green-200 sticky top-0 bg-ebony-950'>
+                  <th className='w-1/6 text-center'>Emoji</th>
                   <th className='w-2/3 text-left'>Name</th>
-                  <th className='w-1/2 text-left'>Importance</th>
+                  <th className='w-1/6 text-left'>Importance</th>
                 </tr>
               </thead>
             </table>
-            <div className='overflow-y-auto w-full' style={{ maxHeight: 'calc(100vh - 300px)' }}>
+            <table className='text-lunar-green-300 border-collapse w-full'>
+              <tbody>
+                <tr className='border-b border-lunar-green-200 h-12'>
+                  <td className='text-right'>
+                    <input 
+                      type='text' 
+                      value={newEmoji} 
+                      placeholder='Add an emoji...' 
+                      className='rounded-sm w-full bg-transparent text-lunar-green-100'
+                      onFocus={() => setShowEmojiPicker(true)}
+                      readOnly // Add this if you want the field to be read-only
+                    />
+                    {showEmojiPicker && (
+                      <div onClick={closeModal} style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
+                        <EmojiPicker onEmojiClick={onEmojiClick} />
+                      </div>
+                    )}
+                  </td>
+                  <td className='text-right'>
+                    <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)} placeholder='Add a name...' className='rounded-sm w-full bg-transparent text-lunar-green-100'/>
+                  </td>
+                  <td className='text-right'>
+                    <input type='number' value={newImportance} onChange={(e) => setNewImportance(e.target.value)} placeholder='Set an importance...' className='rounded-sm w-full bg-transparent text-lunar-green-100' />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div className='overflow-y-auto w-full' style={{ maxHeight: 'calc(100vh - 400px)' }}>
               <table className='text-lunar-green-300 border-collapse w-full'>
                 <tbody className='text-lunar-green-100'>
                   {categories.map((category, index) => (
                     <tr key={index} className='border-b border-lunar-green-200 h-12' onClick={() => handleEditCategory(category)}>
-                      <td className='border-r border-lunar-green-200 w-2/3'>{category.emoji + ' ' + category.name}</td>
-                      <td className='pl-2 w-1/2'>{category.importance}</td>
+                      <td className='w-1/6 text-center'>{category.emoji}</td>
+                      <td className='border-r border-lunar-green-200 w-3/5 text-left'>{category.name}</td>
+                      <td className='w-1/3 text-center'>{category.importance}</td>
                     </tr>
                   ))}
                 </tbody>
               </table>
             </div>
-        </div>
-      )}
+    </div>
       {/* <button className='w-full bg-revolver-300 hover:bg-revolver-400 rounded-lg px-2 h-6 mt-2' onClick={() => HandleClickAddCategory()}>{showInputs ? 'Close' : 'Add Category'}</button> */}
       {!showInputs && upsertError && (<div className='text-red-500'>{upsertError}</div>)}
       {showInputs && (
         <>
         <table className='w-full my-2'>
           <tbody>
-            <tr className='border-b border-lunar-green-200 h-12'>
-              <th className='text-left text-lunar-green-100 w-1/2'>Name</th>
-              <td className='text-right'>
-                <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)} placeholder='Add a name...' className='rounded-sm w-full bg-transparent text-lunar-green-100'/>
-              </td>
-            </tr>
+ 
             <tr className='border-b border-lunar-green-200 h-12'>
               <th className='text-left text-lunar-green-100'>Importance</th>
-              <td className='text-right'>
-                <input type='number' value={newImportance} onChange={(e) => setNewImportance(e.target.value)} placeholder='Set a importance...' className='rounded-sm w-full bg-transparent text-lunar-green-100' />
-              </td>
             </tr>
             <tr className='border-b border-lunar-green-200 h-12'>
               <th className='text-left text-lunar-green-100'>Emoji</th>
-              <td className='text-right'>
-                <input 
-                  type='text' 
-                  value={newEmoji} 
-                  placeholder='Add an emoji...' 
-                  className='rounded-sm w-full bg-transparent text-lunar-green-100'
-                  onFocus={() => setShowEmojiPicker(true)}
-                  readOnly // Add this if you want the field to be read-only
-                />
-                {showEmojiPicker && (
-                  <div onClick={closeModal} style={{ position: 'fixed', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
-                    <EmojiPicker onEmojiClick={onEmojiClick} />
-                  </div>
-                )}
-              </td>
+              
             </tr>
           </tbody>
         </table>
