d606461c9fa68283ed672d2bf03a2e35b5938624
gavacq <8844891+gavacq@users.noreply.github.com>
2024-03-02 09:19:10 -0800

    reduce size of user icon

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -33,7 +33,7 @@ const ProfileMenu = () => {
 
   return (
     <>
-      <FontAwesomeIcon icon={faCircleUser} size="2xl" className="text-lunar-green-300" onClick={() => setShowMenu(true)}/>
+      <FontAwesomeIcon icon={faCircleUser} size="xl" className="text-lunar-green-300" onClick={() => setShowMenu(true)}/>
       {showMenu && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50" onClick={() => setShowMenu(false)}>
           <div className="bg-ebony-950 w-fit h-48 rounded-lg absolute top-4 right-4 flex flex-col justify-center items-center px-6 space-y-2" onClick={e => e.stopPropagation()}>
@@ -48,7 +48,7 @@ const ProfileMenu = () => {
             {currentUser ? (
               <button className="text-white w-full my-2 py-1 px-2 bg-revolver-900 rounded-md" onClick={() => { handleSignOut()}}>Sign Out</button>) : (
               <button onClick={() => handleLogin()} className="bg-gradient-to-r from-revolver-800 to-ebony-800 text-white font-bold py-2 px-4 rounded h-10 w-75 flex items-center justify-center">
-                <FontAwesomeIcon icon={faGoogle} className="mr-2" />
+                <FontAwesomeIcon icon={faGoogle} className="mr-2"/>
                 Sign-in with Google
               </button>
             )}
