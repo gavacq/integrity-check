fdd25f01d505e3d9976098cd7a8ae6d960b43e49
gavacq <8844891+gavacq@users.noreply.github.com>
2024-03-05 12:49:07 -0800

    lint

--- app/api/feedback/route.ts
+++
@@ -1,8 +0,0 @@
-// import nodemailer from 'nodemailer';
-
-// export async function POST(req: Request) {
-
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-
-// }
--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -2,10 +2,6 @@ import FeedbackForm from "./FeedbackForm";
 
 export default function Page () {
   return (
-    <div>
-      <h1>Feedback</h1>
-      <p>Send us your feedback!</p>
-      <FeedbackForm />
-    </div>
+    <div/>
   );
 }
