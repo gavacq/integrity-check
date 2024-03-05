a5abdb31ea2488313cf9c517e6cbd9a1ad3f0d05
gavacq <8844891+gavacq@users.noreply.github.com>
2024-03-05 14:29:33 -0800

    add welcome text

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -3,9 +3,28 @@ import LoginButton from './LoginButton';
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center grow">
-      <h1 className="text-lunar-green-300 mb-4 text-2xl">Welcome to Integrity Check</h1>
-      <LoginButton />
+    <div className="flex flex-col items-center justify-center grow text-lunar-green-300">
+      <div className="flex flex-col items-center justify-center w-4/5">
+        <div className="flex flex-col">
+          <h1 className="mb-4 text-2xl font-bold">
+            Welcome to Integrity Check!
+          </h1>
+          <p className="text-md mb-8">
+            Quantify and analyze your personal growth through daily integrity
+            ratings
+          </p>
+          <LoginButton />
+        </div>
+      </div>
+      <div className="w-full absolute bottom-10">
+        <p className="text-center text-lg text-gray-500 font-light">
+          <strong>Integrity</strong> | /ɪnˈtɛɡrɪti/ <br />
+          noun <br />
+          {`The quality or state of being in alignment with one's values and
+          principles, especially in decision-making and actions. `}
+          <br />
+        </p>
+      </div>
     </div>
   );
 }
